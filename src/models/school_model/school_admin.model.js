2f8dfcd9446e011aa461a13daff39da9855cef82
Moiz-Maskan-Tech <moiz.rehman@maskantech.com>
2025-02-17 11:16:48 -0800

    update Readme.md with new content

--- src/models/school_model/school_admin.model.js
+++ src/models/school_model/school_admin.model.js
@@ -10,33 +10,63 @@ const SchoolAdmin = sequelize.define(
       allowNull: false,
       primaryKey: true,
       autoIncrement: true,
-      unique: true
+      unique: true,
     },
-    //  this is the foreign key 
-    principal_id_fk : {
+    //  this is the foreign key
+    principal_id_fk: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: 'school_principal',
+        key: 'principal_id'
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE",
     },
     // this is also the foreign key
-    admin_role_id_fk : {
+    admin_role_id_fk: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references:{
+        model :'user_role',
+        key: 'user_role_id'
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE"
     },
     // this is the foreign key for schools
     school_id_fk: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references: {
+        model: 'school',
+        key: 'school_id'
+      },
+      onDelete : "CASCADE",
+      onUpdate: "CASCADE"
     },
     // Here we will strore the arrays of the teachers id enrolled by this admin or under this admin
-    teachers_id_fk:{
+    teachers_id_fk: {
       type: DataTypes.INTEGER,
-      allowNull: false
+      allowNull: false,
+      references:{
+        model: '',
+        key: ''
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE"
     },
-    // foreign key for the HOD under this admin 
-    hod_id_fk:{
+    // foreign key for the HOD under this admin
+    hod_id_fk: {
       type: DataTypes.INTEGER,
-      allowNull: false
-    }
+      allowNull: false,
+      references: {
+        model: 'school_hod',
+        key: 'school_hod_id'
+      },
+      onDelete: "CASCADE",
+      onUpdate: "CASCADE"
+    },
   },
   {
     tableName: "school_admin",
